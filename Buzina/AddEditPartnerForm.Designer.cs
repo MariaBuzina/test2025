5e1d2403efe2a2faa26a07ea24bcfb83f472ffce
Buzina <mariabuzinaa@yandex.ru>
2025-04-11 14:17:17 +0300

    Реализована функция добавления партнера

--- Buzina/AddEditPartnerForm.Designer.cs
+++ Buzina/AddEditPartnerForm.Designer.cs
@@ -65,6 +65,7 @@ namespace Buzina
             this.button1.TabIndex = 1;
             this.button1.Text = "Добавить";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
@@ -187,6 +188,7 @@ namespace Buzina
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(510, 36);
             this.textBox2.TabIndex = 14;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
             // 
             // textBox3
             // 
@@ -198,6 +200,7 @@ namespace Buzina
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(449, 107);
             this.textBox3.TabIndex = 15;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
             // 
             // textBox4
             // 
@@ -208,6 +211,7 @@ namespace Buzina
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(434, 36);
             this.textBox4.TabIndex = 16;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
             // 
             // textBox6
             // 
@@ -218,6 +222,8 @@ namespace Buzina
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(434, 36);
             this.textBox6.TabIndex = 18;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            this.textBox6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox6_KeyPress);
             // 
             // maskedTextBox1
             // 
--- Buzina/AddEditPartnerForm.cs
+++ Buzina/AddEditPartnerForm.cs
@@ -15,6 +15,7 @@ namespace Buzina
         public AddEditPartnerForm()
         {
             InitializeComponent();
+            FillPartnerType();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -25,5 +26,121 @@ namespace Buzina
             this.Close();
         }
 
+        /// <summary>
+        /// Заполнение comboBox типом партнера
+        /// </summary>
+        private void FillPartnerType()
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(Connection.con);
+                connection.Open();
+                MySqlCommand command = new MySqlCommand("SELECT partnertypeID, partnertypeTitle FROM partnertype", connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                comboBox1.DataSource = table;
+                comboBox1.DisplayMember = "partnertypeTitle";
+                comboBox1.ValueMember = "partnertypeID";
+
+                connection.Close();
+
+                comboBox1.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar)))
+                e.Handled = true;
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || (e.KeyChar >= 'А' && e.KeyChar <= 'я') || ".,".Contains(e.KeyChar)))
+                e.Handled = true;
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || (e.KeyChar >= 'А' && e.KeyChar <= 'я') || "-".Contains(e.KeyChar)))
+                e.Handled = true;
+        }
+
+        private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) || Char.IsControl(e.KeyChar) || (e.KeyChar >= 'a' && e.KeyChar <= 'z') || "@.".Contains(e.KeyChar)))
+                e.Handled = true;
+        }
+
+        /// <summary>
+        /// Метод для добавления и редактирования партнера
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox6.Text == "" || comboBox1.SelectedIndex == -1 || maskedTextBox1.MaskFull == false)
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля!", "Внмание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (button1.Text == "Добавить")
+                {
+                    try
+                    {
+                        MySqlConnection connection = new MySqlConnection(Connection.con);
+                        connection.Open();
+                        MySqlCommand command = new MySqlCommand($@"INSERT INTO partner
+                                                                (partnerType,
+                                                                partnerTitle,
+                                                                partnerDirector,
+                                                                partnerEmail, 
+                                                                partnerPhone,
+                                                                partnerAddress, 
+                                                                partnerRating)
+                                                                VALUES (
+                                                                '{comboBox1.SelectedValue}', 
+                                                                '{textBox1.Text}', 
+                                                                '{textBox4.Text}', 
+                                                                '{textBox6.Text}', 
+                                                                '{maskedTextBox1.Text}', 
+                                                                '{textBox3.Text}', 
+                                                                '{textBox2.Text}')", connection);
+                        command.ExecuteNonQuery();
+                        connection.Close();
+
+                        MessageBox.Show("Запись успешно добавлена", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        textBox1.Clear();
+                        textBox2.Clear();
+                        textBox3.Clear();
+                        textBox4.Clear();
+                        textBox6.Clear();
+                        maskedTextBox1.Clear();
+                        comboBox1.SelectedIndex = -1;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                   
+                }
+            }
+        }
     }
 }
