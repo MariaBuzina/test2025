d7bceb5fc20de4470e119e814dc771d44a25df46
Buzina <mariabuzinaa@yandex.ru>
2025-04-11 13:38:48 +0300

    Создан проект с unit тестами для тестирования метода подсчета скидки

---
+++ Buzina/BuzinaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BuzinaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BuzinaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c4f8576-6cd9-4313-b8b5-7d50888be87b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Buzina/BuzinaTest/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Buzina;
+
+namespace BuzinaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Discount discount = new Discount();
+
+        [TestMethod]
+        public void GetDiscount0()
+        {
+            int expected = 0;
+
+            int count = 5000;
+            int actual = discount.GetDiscount(count);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetDiscount5()
+        {
+            int expected = 5;
+
+            int count = 15000;
+            int actual = discount.GetDiscount(count);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetDiscount10()
+        {
+            int expected = 10;
+
+            int count = 55000;
+            int actual = discount.GetDiscount(count);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetDiscount15()
+        {
+            int expected = 15;
+
+            int count = 355000;
+            int actual = discount.GetDiscount(count);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetDiscount0WithNegativeNumber()
+        {
+            int expected = 0;
+
+            int count = -1500;
+            int actual = discount.GetDiscount(count);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
