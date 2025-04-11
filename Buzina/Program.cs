62e65c3c03e3c6fcca26b40e7ca78e85fffac1dd
Buzina <mariabuzinaa@yandex.ru>
2025-04-11 13:31:58 +0300

    Реализован класс для получения скидки партнера

--- Buzina/Program.cs
+++ Buzina/Program.cs
@@ -16,7 +16,7 @@ namespace Buzina
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
